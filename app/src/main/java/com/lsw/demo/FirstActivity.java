a0969784b205cc36fb06baaf3fafdb633b5e2ea6
liushuwei <liushuwei@qiyi.com>
2017-08-31 19:16:12 +0800

    修改Activity Demo

---
+++ app/src/main/java/com/lsw/demo/FirstActivity.java
@@ -0,0 +1,36 @@
+package com.lsw.demo;
+
+import android.content.Intent;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class FirstActivity extends AppCompatActivity {
+
+    private Button mButton;
+    private static final String TAG = "FirstActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+        mButton = (Button)findViewById(R.id.button1);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        Log.i(TAG, "onSaveInstanceState: ");
+    }
+}
